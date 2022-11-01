25bddf60943f4f35c16d60cf529bf8cf338e3213
jasonmg <jasonmg@mit.edu>
2022-11-01 13:50:57 -0400

    Cleanup.

--- client/components/Bookmark/BookmarkButton.vue
+++ client/components/Bookmark/BookmarkButton.vue
@@ -29,7 +29,7 @@ export default {
   data() {
     return {
       bookmarked: false, // Whether this freet is bookmarked by this user
-      bookmarkId: '',
+      bookmarkId: '', // The bookmarkId, if this freet is bookmarked by this user
     }
   },
   async mounted() {
@@ -44,16 +44,8 @@ export default {
       if (bookmarkResponse.freet === this.freet.id) {
         this.bookmarked = true;
         this.bookmarkId = bookmarkResponse._id;
-        console.log('setting bookmarkId to', bookmarkResponse._id);
       }
     }
-
-    console.log('freetId', this.freet.id);
-    console.log('res', res);
-    console.log('res._id', res._id);
-    console.log('bookmarked', this.bookmarked);
-    console.log('bookmarkId', this.bookmarkId);
-
   },
   methods: {
     async addBookmark() {
@@ -79,7 +71,6 @@ export default {
 
       this.bookmarked = true;
       this.bookmarkId = res.bookmark._id;
-      console.log('on add bookmark, setting bookmarkId to', this.bookmarkId)
     },
     removeBookmark() {
       const params = {
@@ -90,28 +81,9 @@ export default {
           });
         }
       };
-      console.log('calling delete bookmark with bookmark id', this.bookmarkId);
       this.request(params);
       this.bookmarked = false;
       this.bookmarkId = '';
-    },
-    async checkBookmarkStatus() {
-      // Check whether this freet is bookmarked by this user
-      const options = {
-        method: 'GET', headers: {'Content-Type': 'application/json'}
-      };
-
-      const res = await fetch(`/api/bookmark?username=${this.$store.state.username}`, options).then(async r => r.json());
-
-      for (const bookmarkResponse in res) {
-        if (bookmarkResponse.freet === this.freet.id) {
-          this.bookmarked = true;
-        }
-      }
-
-    console.log('freetId', this.freet.id);
-    console.log('res', res);
-    console.log('bookmarked', this.bookmarked);
     },
     async request(params) {
       /**
@@ -136,49 +108,12 @@ export default {
           throw new Error(res.error);
         }
 
-        // this.editing = false;
-        // this.$store.commit('refreshFreets');
-
         params.callback();
       } catch (e) {
         this.$set(this.alerts, e, 'error');
         setTimeout(() => this.$delete(this.alerts, e), 3000);
       }
     }
-    
-    
-    
-    
-    // async request(params) {
-    //   /**
-    //    * Submits a request to the bookmark endpoint
-    //    * @param params - Options for the request
-    //    * @param params.body - Body for the request, if it exists
-    //    * @param params.callback - Function to run if the the request succeeds
-    //    */
-    //   const options = {
-    //     method: params.method, headers: {'Content-Type': 'application/json'}
-    //   };
-    //   if (params.body) {
-    //     options.body = params.body;
-    //   }
-
-    //   try {
-    //     const r = await fetch(`/api/bookmark/`, options);
-    //     if (!r.ok) {
-    //       const res = await r.json();
-    //       throw new Error(res.error);
-    //     }
-
-    //     // this.editing = false;
-    //     this.$store.commit('refreshFreets');
-
-    //     params.callback();
-    //   } catch (e) {
-    //     this.$set(this.alerts, e, 'error');
-    //     setTimeout(() => this.$delete(this.alerts, e), 3000);
-    //   }
-    // }
   }
 };
 </script>
